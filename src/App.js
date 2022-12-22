0f2c9c6c71236b687fa3e7d1a7c4d8750fcc3e3e
Arda Günal <ardathegunal@gmail.com>
2022-12-22 22:48:50 +0300

    SSH Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import useWindowDimensions from "./Tools/windowDimensions";
 
 function App() {
+
+    const { height, width } = useWindowDimensions();
+    const [showProjects, setShowProjects] = useState(false);
+
+    const expand = (event) => {event.target.style.scale = (2)}
+    const shrink = (event) => {event.target.style.scale = (1)}
+
+    const MainCircleClick = () => {
+        let elements = document.getElementsByClassName("CircleClickable")
+
+        let position = {transform: "translateX(100px)"}
+        for (let i = 0; i < elements.length; i++){
+            elements[i].style = position
+        setShowProjects(true)
+    }}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="App-body">
+
+        <a className="ClickMe"
+            hidden={showProjects}
+            onMouseEnter={expand}
+            onMouseLeave={shrink}
+            onClick={MainCircleClick}>
         </a>
-      </header>
+
+          <a className="CircleClickable"
+             hidden={!showProjects}
+             onMouseEnter={expand}
+             onMouseLeave={shrink}>
+          </a>
+
+          <a className="CircleClickable"
+             hidden={!showProjects}
+             onMouseEnter={expand}
+             onMouseLeave={shrink}>
+          </a>
+
+          <a className="CircleClickable"
+             hidden={!showProjects}
+             onMouseEnter={expand}
+             onMouseLeave={shrink}>
+          </a>
+
+          <a className="CircleClickable"
+             hidden={!showProjects}
+             onMouseEnter={expand}
+             onMouseLeave={shrink}>
+          </a>
+
+      </div>
     </div>
   );
 }
